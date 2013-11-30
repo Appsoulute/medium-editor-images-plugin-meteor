b2829f8a026b7bb31bbd46ee96f78b89072132a6
JAEHO LEE <spectrick@gmail.com>
2013-12-01 03:59:57 +0900

    remove jquery1.10. use jquery package. p margin patch

--- package.js
+++ package.js
@@ -5,11 +5,9 @@ Package.describe({
  git submodule add https://github.com/acidsound/medium-editor-images-plugin.git medium-editor-images-plugin
  */
 Package.on_use(function (api) {
-  api.use('jquery', 'client');
   api.add_files([
     'medium-editor-images-plugin/src/medium-editor/css/medium-editor.css',
     'medium-editor-images-plugin/src/medium-editor/js/medium-editor.js',
-    'medium-editor-images-plugin/src/medium-editor-images-plugin/js/jquery-1.10.2.min.js',
     'medium-editor-images-plugin/src/medium-editor-images-plugin/images/remove.png',
     'medium-editor-images-plugin/src/medium-editor-images-plugin/images/resize-bigger.png',
     'medium-editor-images-plugin/src/medium-editor-images-plugin/images/resize-smaller.png',
